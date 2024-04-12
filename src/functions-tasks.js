6569e1ce71d03aaff42773576be01833fff621ee
Sergey <seleznev-krista@mail.ru>
2024-04-12 11:43:05 +0300

    feat: add getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -70,8 +70,9 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const power = exponent;
+  return (x) => x ** power;
 }
 
 /**
