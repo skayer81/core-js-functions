98f75f3ad0f43de1297776a128d1447287eed390
Sergey <seleznev-krista@mail.ru>
2024-04-12 14:27:30 +0300

    feat: add partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -171,9 +171,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
-}
+const partialUsingArguments =
+  (fn, ...args1) =>
+  (...args) =>
+    args1.join('') + args.join('');
 
 /**
  * Returns the id generator function that returns next integer starting
